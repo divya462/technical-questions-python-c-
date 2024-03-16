1a260b3c640cd38d22df16eee790ec653c586269
Divya kotte <153913545+divya462@users.noreply.github.com>
2024-03-16 21:25:02 +0530

    Create anagram.py

---
+++ anagram.py
@@ -0,0 +1,16 @@
+str1=input()
+str2=input()
+lst1=[]
+lst2=[]
+Flag=True
+for i in str1:
+     lst1.append(i)
+for i in str2:
+    lst2.append(i)
+if i in lst1:
+     if i not in lst2:
+       Flag=False
+if(Flag==True):
+      print("anagram")
+else:
+    print("not anagram")
